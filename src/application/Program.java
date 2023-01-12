fbe52741f4db4048faf7a8859fb9245363bb1f04
michelsonron7 <michelsonroncete@gmail.com>
2023-01-12 20:44:58 -0300

    Implementing possible moves of king

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,14 +8,13 @@ import chess.ChessMath;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
-
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		ChessMath chessMath = new ChessMath();
-		
+
 		while (true) {
 			try {
 				UI.clearScreen();
@@ -23,27 +22,25 @@ public class Program {
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
-				
+
 				boolean[][] possibleMoves = chessMath.possibleMoves(source);
 				UI.clearScreen();
 				UI.printBoard(chessMath.getPiece(), possibleMoves);
-				
+
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
-				
-				ChessPiece capturedPiece = chessMath.performChessMove(source, target);
-			}
-			catch (ChessException e) {
+
+			ChessPiece capturedPiece = chessMath.performChessMove(source, target);
+			} catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			} catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
-			catch (InputMismatchException e) {
-				System.out.println(e.getMessage());
-				sc.nextLine(); 
-			}
 		}
-		
+
 	}
 
 }
